15b8d6cb9937e9d93ed98de09d5245dbb47844b8
Manuel Palumbo <manuel.palumbo@email.com>
2023-03-12 17:05:31 +0100

    Fix: Inventory not saved on restart when cached

--- src/main/java/ml/empee/lockbox/services/VaultService.java
+++ src/main/java/ml/empee/lockbox/services/VaultService.java
@@ -2,14 +2,15 @@ package ml.empee.lockbox.services;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.RemovalCause;
 import com.google.common.cache.RemovalListener;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import ml.empee.ioc.Bean;
 import ml.empee.lockbox.exceptions.VaultUnauthorizedException;
 import ml.empee.lockbox.model.Vault;
 import ml.empee.lockbox.model.Vaults;
 import ml.empee.lockbox.repositories.VaultRepository;
+import ml.empee.lockbox.utils.helpers.Logger;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -18,32 +19,35 @@ import org.bukkit.inventory.Inventory;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+@RequiredArgsConstructor
 public class VaultService implements Bean {
 
   private final VaultRepository repository;
-  private final Cache<Block, Inventory> vaultStorageCache;
+  private final Logger logger;
 
-  public VaultService(VaultRepository repository) {
-    this.repository = repository;
+  private Cache<Block, Inventory> vaultStorageCache = CacheBuilder.newBuilder().build();
 
-    //Necessary to do in constructor bc the removal listener is using a reference to the repository
+  @Override
+  public void onStart() {
     vaultStorageCache = CacheBuilder.newBuilder()
         .removalListener(onCacheExpire())
         .expireAfterAccess(10, TimeUnit.SECONDS)
         .build();
   }
 
+  @Override
+  public void onStop() {
+    vaultStorageCache.invalidateAll();
+  }
+
   private RemovalListener<Block, Inventory> onCacheExpire() {
     return event -> {
-      if(event.getCause() == RemovalCause.EXPLICIT) {
-        return;
+      Vault vault = findVaultAt(event.getKey()).orElse(null);
+      if(vault != null) {
+        repository.setVaultStorage(vault, event.getValue().getStorageContents());
+      } else {
+        logger.debug("Unable to save content of cached vault at " + event.getKey().getLocation());
       }
-
-      Vault vault = findVaultAt(event.getKey()).orElseThrow(
-          () -> new IllegalStateException("Unable to save storage, vault not existing")
-      );
-
-      repository.setVaultStorage(vault, event.getValue().getStorageContents());
     };
   }
 
@@ -56,8 +60,11 @@ public class VaultService implements Bean {
   }
   public void destroyVault(Vault vault) {
     vault.despawnDecorations();
+
+    vaultStorageCache.invalidate(vault.getBlock());
     repository.deleteVault(vault);
   }
+
   public Optional<Vault> findVaultAt(Block block) {
     Vaults.Type vaultType = repository.getVaultTypeAt(block).orElse(null);
     if(vaultType == null) {
@@ -81,7 +88,7 @@ public class VaultService implements Bean {
   public Inventory getVaultInventory(Vault vault) {
     return vaultStorageCache.get(vault.getBlock(), () -> {
       Inventory inventory = vault.getInventoryTemplate();
-      inventory.addItem(repository.getVaultStorage(vault));
+      inventory.setContents(repository.getVaultStorage(vault));
       return inventory;
     });
   }
