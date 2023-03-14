7544ef6d03b8ccbdf8419a2e7a7c60303d523f4f
Manuel Palumbo <manuel.palumbo@email.com>
2023-03-14 19:13:17 +0100

    Improvement: Update & Stats system

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "ml.empee"
-version = "1.0.0-SNAPSHOT"
+version = "0.0.1"
 var basePackage = "ml.empee.lockbox"
 
 bukkit {
--- src/main/java/ml/empee/lockbox/LockBox.java
+++ src/main/java/ml/empee/lockbox/LockBox.java
@@ -2,22 +2,24 @@ package ml.empee.lockbox;
 
 import lombok.Getter;
 import ml.empee.ioc.SimpleIoC;
+import ml.empee.lockbox.utils.Metrics;
+import ml.empee.notifier.Notifier;
 
 /** Boot class of this plugin. **/
 
 public final class LockBox extends org.bukkit.plugin.java.JavaPlugin {
 
   public static final String PREFIX = "  &bLockBox &8&l»&r ";
-  private static final String SPIGOT_PLUGIN_ID = "";
-  private static final Integer METRICS_PLUGIN_ID = 0;
+  private static final String SPIGOT_PLUGIN_ID = "108615";
+  private static final Integer METRICS_PLUGIN_ID = 17957;
   @Getter
   private final SimpleIoC iocContainer = new SimpleIoC(this);
 
   public void onEnable() {
     iocContainer.initialize("relocations");
 
-    //Metrics.of(this, METRICS_PLUGIN_ID);
-    //Notifier.listenForUpdates(this, SPIGOT_PLUGIN_ID);
+    Metrics.of(this, METRICS_PLUGIN_ID);
+    Notifier.listenForUpdates(this, SPIGOT_PLUGIN_ID);
   }
 
   public void onDisable() {
